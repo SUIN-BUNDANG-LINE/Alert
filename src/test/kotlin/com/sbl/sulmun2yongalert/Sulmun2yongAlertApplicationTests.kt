5a2088d95e216c3c244b876c59e49dec9ac2a134
JeongHunHui <deca3248@gmail.com>
2025-04-02 12:00:27 +0900

    [SBL-232] Spring Boot 프로젝트 초기 설정

---
+++ src/test/kotlin/com/sbl/sulmun2yongalert/Sulmun2yongAlertApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.sbl.sulmun2yongalert
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Sulmun2yongAlertApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
